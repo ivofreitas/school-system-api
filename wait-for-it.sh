ee9a6e6a2574876dd0f87b2eb530b2f0f4e4e144
Ivo <ivorfn@gmail.com>
2023-07-14 13:02:26 -0300

    dockerized app

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # wait-for-it.sh: A script to wait for TCP services to become available
-echo "start"
+
 set -e
 
 host="$1"
 port="$2"
-echo "start shift"
 shift 2
 cmd="$@"
-echo "end shift"
+
 until nc -z "$host" "$port"; do
   >&2 echo "Waiting for $host:$port to be available..."
   sleep 1
